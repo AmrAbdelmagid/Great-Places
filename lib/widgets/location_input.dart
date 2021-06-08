d89c668f4bc18826123148888142f97d986d18ba
Amr Abdelmagid <amrabdelmagid900@gmail.com>
2021-06-08 05:57:46 +0200

    Added pick location on the map

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:great_places/helpers/location_helper.dart';
 import 'package:great_places/screens/map_screen.dart';
 import 'package:location/location.dart';
@@ -21,13 +24,14 @@ class _InputLocationState extends State<InputLocation> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(MaterialPageRoute(
+    final selectedLocation = await Navigator.of(context).push<LatLng>(MaterialPageRoute(
       fullscreenDialog: true,
       builder: (ctx) => MapScreen(isSelecting: true),
     ));
     if (selectedLocation == null) {
       return;
     }
+    log(selectedLocation.longitude.toString());
   }
 
   @override
